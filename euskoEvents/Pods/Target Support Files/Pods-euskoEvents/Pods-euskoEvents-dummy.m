8b0c272f7fdc1469ea6d2e3ebdac0553d66639d8
Unknown <tehenua@gmail.com>
2017-11-30 21:46:10 +0100

    Inicio

---
+++ euskoEvents/Pods/Target Support Files/Pods-euskoEvents/Pods-euskoEvents-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_euskoEvents : NSObject
+@end
+@implementation PodsDummy_Pods_euskoEvents
+@end
---
+++ euskoEvents/euskoEvents/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  euskoEvents
+//
+//  Created by  on 30/11/17.
+//  Copyright © 2017 TeHenua. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var titulo: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
